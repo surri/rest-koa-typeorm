1dd1d47acba0c921fc96b89d413fd316fb6948dc
surri <thekeh@hotmail.com>
2021-12-07 15:47:58 +0900

    route

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,5 +4,21 @@ import logger from 'koa-logger'
 import json from 'koa-json'
 
 const app = new Koa();
+const router = new Router();
+
+router.get('/ping', function (ctx, next) {
+    ctx.body = {
+        status: 200,
+        data: {
+            foo: 'bar',
+            ping: 'pong',
+        }
+    }
+    return next();
+});
+
+app.use(logger())
+app.use(json({ pretty: true }));
+app.use(router.routes());
 
 app.listen(3000, () => console.log('start server'))
